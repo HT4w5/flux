1ff3f5cf2aaa94cb8c555abd00f096a3a21da757
HT4w5 <ht4w5x@gmail.com>
2026-03-26 16:23:11 +0800

    refactor(parser): implement nocopy and string interning

--- go.mod
+++ go.mod
@@ -3,17 +3,21 @@ module github.com/HT4w5/flux
 go 1.25.8
 
 require (
-	github.com/allegro/bigcache v1.2.1
+	github.com/VictoriaMetrics/fastcache v1.13.3
 	github.com/bytedance/sonic v1.15.0
 	github.com/gaissmai/bart v0.26.1
+	github.com/hashicorp/golang-lru/v2 v2.0.7
 	github.com/valyala/bytebufferpool v1.0.0
 	gopkg.in/mcuadros/go-syslog.v2 v2.3.0
 )
 
 require (
+	github.com/allegro/bigcache v1.2.1 // indirect
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
--- pkg/parser/nginx/nginx.go
+++ pkg/parser/nginx/nginx.go
@@ -1,59 +1,65 @@
 package nginx
 
 import (
-	"fmt"
-	"math"
-	"net/netip"
+	"bytes"
+	"encoding/json"
 	"strconv"
-	"strings"
 	"time"
+	"unsafe"
 
-	json "github.com/bytedance/sonic"
+	"github.com/bytedance/sonic"
+	lru "github.com/hashicorp/golang-lru/v2"
 
 	"github.com/HT4w5/flux/pkg/dto"
 )
 
 const (
-	nsPerS = 1000000000
+	nsPerS = 1e9
 )
 
 type nginxJSONLogEntry struct {
-	Time     string `json:"time"` // $msec format: "1734345934.123"
-	Client   string `json:"client"`
-	Server   string `json:"server"`
-	Method   string `json:"method"`
-	URL      string `json:"url"`
-	Status   int    `json:"status"`
-	Sent     int64  `json:"sent"`
-	Duration string `json:"duration"`
-	Host     string `json:"host"`
-	Agent    string `json:"agent"`
+	Time     sonic.NoCopyRawMessage `json:"time"` // $msec format: "1734345934.123"
+	Client   sonic.NoCopyRawMessage `json:"client"`
+	Server   sonic.NoCopyRawMessage `json:"server"`
+	Method   sonic.NoCopyRawMessage `json:"method"`
+	URL      sonic.NoCopyRawMessage `json:"url"`
+	Status   int                    `json:"status"`
+	Sent     int64                  `json:"sent"`
+	Duration sonic.NoCopyRawMessage `json:"duration"`
+	Host     sonic.NoCopyRawMessage `json:"host"`
+	Agent    sonic.NoCopyRawMessage `json:"agent"`
 }
 
-type NginxJSONParser struct{}
+type NginxJSONParser struct {
+	decoder   json.Decoder
+	methodLRU *lru.TwoQueueCache[string, string]
+	urlLRU    *lru.TwoQueueCache[string, string]
+	hostLRU   *lru.TwoQueueCache[string, string]
+	agentLRU  *lru.TwoQueueCache[string, string]
+}
 
 func (p *NginxJSONParser) Parse(line []byte) (dto.Request, error) {
 	var logEntry nginxJSONLogEntry
-	err := json.Unmarshal(line, &logEntry)
+	err := sonic.Unmarshal(line, &logEntry)
 	if err != nil {
 		return dto.Request{}, err
 	}
 
 	r := dto.Request{
-		Method: logEntry.Method,
-		URL:    logEntry.URL,
+		Method: internMethod(logEntry.Method),
+		URL:    internString(p.urlLRU, logEntry.URL),
 		Status: logEntry.Status,
 		Sent:   logEntry.Sent,
-		Host:   logEntry.Host,
-		Agent:  logEntry.Agent,
+		Host:   internString(p.hostLRU, logEntry.Host),
+		Agent:  internString(p.agentLRU, logEntry.Agent),
 	}
 
 	r.Time, err = parseNginxTime(logEntry.Time)
-	r.Client, err = netip.ParseAddr(logEntry.Client)
+	err = r.Client.UnmarshalText(logEntry.Client)
 	if err != nil {
 		return dto.Request{}, err
 	}
-	r.Server, err = netip.ParseAddr(logEntry.Server)
+	err = r.Server.UnmarshalText(logEntry.Server)
 	if err != nil {
 		return dto.Request{}, err
 	}
@@ -62,38 +68,92 @@ func (p *NginxJSONParser) Parse(line []byte) (dto.Request, error) {
 	return r, nil
 }
 
-func parseNginxTime(tStr string) (time.Time, error) {
-	dotIdx := strings.IndexByte(tStr, '.')
-	if dotIdx < 0 {
-		return time.Time{}, fmt.Errorf("bad time format")
+// Intern helpers
+func internString(lru *lru.TwoQueueCache[string, string], b []byte) string {
+	lookupKey := unsafe.String(unsafe.SliceData(b), len(b))
+	// Cache hit
+	if val, ok := lru.Get(lookupKey); ok {
+		return val
 	}
 
-	i, err := strconv.ParseInt(tStr[:dotIdx], 10, 64)
-	if err != nil {
-		return time.Time{}, err
-	}
-	fStr := tStr[dotIdx+1:]
-	f, err := strconv.ParseInt(fStr, 10, 64)
-	if err != nil {
-		return time.Time{}, err
-	}
-	return time.Unix(i, f*nsPerS/int64(math.Pow10(len(fStr)))), nil
+	// Cache miss, make copy
+	permString := string(b)
+	lru.Add(permString, permString)
+	return permString
 }
 
-func parseNginxDuration(dStr string) (time.Duration, error) {
-	dotIdx := strings.IndexByte(dStr, '.')
+var commonMethods = map[string]string{
+	"GET":     "GET",
+	"HEAD":    "HEAD",
+	"POST":    "POST",
+	"PUT":     "PUT",
+	"DELETE":  "DELETE",
+	"CONNECT": "CONNECT",
+	"OPTIONS": "OPTIONS",
+	"TRACE":   "TRACE",
+	"PATCH":   "PATCH",
+}
+
+func internMethod(b []byte) string {
+	s := unsafe.String(unsafe.SliceData(b), len(b))
+	if val, ok := commonMethods[s]; ok {
+		return val
+	}
+	return string(b) // Fallback for uncommon methods
+}
+
+// Parser helpers
+
+var pow10 = [10]int64{1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000}
+
+func parseNginxDuration(b []byte) (time.Duration, error) {
+	dotIdx := bytes.IndexByte(b, '.')
 	if dotIdx < 0 {
-		return 0, fmt.Errorf("bad duration format")
+		sec, err := strconv.ParseInt(unsafe.String(&b[0], len(b)), 10, 64)
+		return time.Duration(sec * nsPerS), err
 	}
 
-	i, err := strconv.ParseInt(dStr[:dotIdx], 10, 64)
+	// Parse seconds (Unix Epoch)
+	sec, err := strconv.ParseInt(unsafe.String(&b[0], dotIdx), 10, 64)
 	if err != nil {
 		return 0, err
 	}
-	fStr := dStr[dotIdx+1:]
-	f, err := strconv.ParseInt(fStr, 10, 64)
+
+	// Parse milliseconds/microseconds
+	fracBytes := b[dotIdx+1:]
+	frac, err := strconv.ParseInt(unsafe.String(&fracBytes[0], len(fracBytes)), 10, 64)
 	if err != nil {
 		return 0, err
 	}
-	return time.Duration(i*nsPerS + f*nsPerS/int64(math.Pow10(len(fStr)))), nil
+
+	// Convert fraction to nanoseconds
+	nsec := (frac * 1e9) / pow10[len(fracBytes)]
+
+	return time.Duration(sec*nsPerS + nsec), nil
+}
+
+func parseNginxTime(b []byte) (time.Time, error) {
+	dotIdx := bytes.IndexByte(b, '.')
+	if dotIdx < 0 {
+		sec, err := strconv.ParseInt(unsafe.String(&b[0], len(b)), 10, 64)
+		return time.Unix(sec, 0), err
+	}
+
+	// Parse seconds (Unix Epoch)
+	sec, err := strconv.ParseInt(unsafe.String(&b[0], dotIdx), 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	// Parse milliseconds/microseconds
+	fracBytes := b[dotIdx+1:]
+	frac, err := strconv.ParseInt(unsafe.String(&fracBytes[0], len(fracBytes)), 10, 64)
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	// Convert fraction to nanoseconds
+	nsec := (frac * nsPerS) / pow10[len(fracBytes)]
+
+	return time.Unix(sec, nsec), nil
 }
