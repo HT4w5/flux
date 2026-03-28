ae4b92f8f36d3a1da3ca4808842f1fddebe03bf6
HT4w5 <ht4w5x@gmail.com>
2026-03-28 18:26:44 +0800

    feat(dto): add BanRule

--- pkg/dto/ban.go
+++ pkg/dto/ban.go
@@ -5,10 +5,16 @@ import (
 	"time"
 )
 
-// Ban entry
-type Ban struct {
-	ID        int64
-	Prefix    netip.Prefix
-	Blame     string
-	ExpiresAt time.Time
+// Ban record stored in jail
+type BanRecord struct {
+	ID        int64        `json:"id"`
+	Prefix    netip.Prefix `json:"prefix"`
+	Blame     string       `json:"blame"`
+	ExpiresAt time.Time    `json:"expires_at"`
+}
+
+// Ban rule compiled by jail
+type BanRule struct {
+	Prefixes []netip.Prefix `json:"prefixes"`
+	DstPorts []uint16       `json:"dst_ports"`
 }
